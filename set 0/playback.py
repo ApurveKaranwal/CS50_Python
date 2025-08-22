cc93acfcd5c1681319f9dfbf1edb617f87348ffd
ApurveKaranwal <apurvekaranwal282@gmail.com>
2025-08-22 10:22:50 +0530

    fnothing

--- set 0/playback.py
+++ set 0/playback.py
@@ -1,4 +1,5 @@
 #In a file called playback.py, implement a program in Python that prompts the user for input and then outputs that same input, replacing each space with ... (i.e., three periods).
 
 lecture = input("tell me something :")
-new=lecture.replace("", ...)
+new=lecture.replace(" ", "...")
+print(new)
