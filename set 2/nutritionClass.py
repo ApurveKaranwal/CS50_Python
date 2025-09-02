575773847600eafb5266ef386052724fae328372
ApurveKaranwal <apurvekaranwal282@gmail.com>
2025-09-02 14:12:27 +0530

    Refactor nutrition.py to use class-based structure

---
+++ set 2/nutritionClass.py
@@ -0,0 +1,39 @@
+#making nutrition.py but by using class
+class fruitNutrition():
+    def __init__(self):
+        self.fruitData={
+            "apple": 130,
+    "avocado california": 50,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plums": 70,
+    "strawberries": 50,
+    "sweet cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80,
+        }
+    def Calories(self,fruitName):
+        fruitName=fruitName.lower().strip()
+        if fruitName in self.fruitData:
+            print(f"Calories: {self.fruitData[fruitName]}")
+
+def main():
+    name=input("Item: ")
+    fruitNutrition().Calories(name)
+
+if __name__ == "__main__":
+    main()
+
+
+        
